ad5f4e2495ac80e124552f64f53c5b1151c6d0a5
Karl Fischer <karl.fischer@project-a.com>
2016-10-02 11:15:54 +0200

    Small fix

--- admin/build.py
+++ admin/build.py
@@ -8,7 +8,7 @@ APP_TYPE = 'admin'
 APP_REPO = 'app/admin'
 
 
-def build_image(branch_name, hub, repo, hub_user, hub_pass):
+def build_image(branch_name, hub, hub_user, hub_pass):
     c.build_branch(branch_name, APP_TYPE)
     c.docker_login(hub_user, hub_pass, hub)
     c.build_image(branch_name, APP_TYPE, hub, APP_REPO)
