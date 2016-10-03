c9bd6099ed41f8ccf6aaa400eb75b51f8a44e0c5
Karl Fischer <karl.fischer@project-a.com>
2016-10-03 22:25:08 +0200

    Add mobile

---
+++ mobile/build.py
@@ -0,0 +1,14 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import common as c
+
+
+APP_TYPE = 'mobile'
+APP_REPO = 'app/mobile'
+
+
+def build_image(branch_name, hub, hub_user, hub_pass, build_host, fab_root):
+    c.build_branch(branch_name, APP_TYPE)
+    c.build_image(branch_name, APP_TYPE, hub, APP_REPO, build_host,
+                  hub_user, hub_pass, fab_root)
---
+++ mobile/deploy.py
@@ -0,0 +1,14 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import common as c
+
+
+APP_TYPE = 'mobile'
+APP_REPO = 'app/mobile'
+NGINX_IMAGE = 'nginx:1.11.4-alpine'
+
+
+def deploy_image(branch_name, hub, hub_user, hub_pass, mongo_url, root_url):
+    c.deploy_image(branch_name, APP_REPO, NGINX_IMAGE, hub, hub_user,
+                   hub_pass, APP_TYPE, mongo_url, root_url)
